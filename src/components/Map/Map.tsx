2fe17471081a25144eb7869a0a36ed8af9189359
sitl_idnm <klevtsevitch.lev@yandex.ru>
2024-09-03 16:39:43 +0300

    feat: wipe global paddings

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,12 +3,10 @@ import styles from './styles.module.css';
 export const Map = () => {
   return (
 		<section className={styles.wrapper}>
-			<div className={styles.contreWrapper}>
 				<div className={styles.titleWrapper}>
 				<h2 className={styles.title}>Хотите стать<br />одним из нас? </h2>
 				<p className={styles.description}>Приглашаем в команду</p>
 				</div>
-			</div>
     </section>
   );
 };
