14962a29e970c2a1420e8254e5866acbd568e098
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-04 18:05:21 +0300

    feat: TextAnimation/Add motion for anim

--- src/components/TextAnimation/TextAnimation.tsx
+++ src/components/TextAnimation/TextAnimation.tsx
@@ -7,11 +7,28 @@ import styles from './styles.module.css';
 
 gsap.registerPlugin(ScrollTrigger)
 
-export function TextAnimation({ text, style }: { text: string, style: any }) {
+export function TextAnimation({ text, style }: { text: any, style: any }) {
 	const TextAnimka = useRef(null)
+	const TextLine = useRef(null)
 
 	const textClass = classNames(styles.Animka, style);
-
+	useGSAP(() => {
+		const Line = TextLine.current
+		const tl = gsap.timeline
+		tl({
+			scrollTrigger: {
+				trigger: Line,
+				scrub: 1,
+				markers: true,
+				start: 'top 100%'
+			}
+		})
+			.fromTo(Line, {
+			x: -200
+		}, {
+			x: 0
+		})
+	})
 	useGSAP(() => {
 		const TextAnima = TextAnimka.current
 		const tl = gsap.timeline
@@ -23,14 +40,22 @@ export function TextAnimation({ text, style }: { text: string, style: any }) {
 				start: 'top 70%'
 			}
 		}).fromTo(TextAnima, {
-			backgroundPositionX: '100%'
+			backgroundPositionX: '100%',
+			x: -100
     },
     {
-			backgroundPositionX: '0%'
+			backgroundPositionX: '0%',
+			x: 0
     })
 	});
 
 	return (
-		<h2 className={textClass} ref={TextAnimka}>{ text }</h2>
+		<h2 className={textClass} ref={TextAnimka}>
+			{Array.isArray(text) ? text.map((line) => (
+				<span>
+					{line} <br />
+				</span>
+			)) : text}
+		</h2>
 	)
 }
