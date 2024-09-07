f374ed9226585db9cfd0aeefe7bbbd7f111a0676
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-07 11:17:07 +0300

    feat: Turos/ add adaptive Torus

--- src/components/Intro/TorusScene/TorusScene.tsx
+++ src/components/Intro/TorusScene/TorusScene.tsx
@@ -1,46 +1,26 @@
-// import { Canvas } from '@react-three/fiber';
-// import { Edges, OrbitControls } from '@react-three/drei';
-// import styles from './styles.module.css';
-// import * as THREE from 'three';
-
-// const Torus = () => {
-//   return (
-//     <group>
-//       <lineSegments>
-//         <edgesGeometry args={[new THREE.TorusGeometry(10, 4, 16, 26)]} />
-//         <lineBasicMaterial color="white" />
-//       </lineSegments>
-//     </group>
-//   );
-// };
-
-// export const TorusScene = () => {
-//   return (
-//     <Canvas
-//       className={styles.hui}
-//       camera={{ position: [10, -20, 20], fov: 75 }}
-//       style={{ height: '600px', width: '700px' }}
-//     >
-//       <ambientLight intensity={0.5} />
-//       <pointLight position={[10, 10, 10]} />
-//       <Torus />
-//       <OrbitControls />
-//     </Canvas>
-//   );
-// };
-
+import styles from './styles.module.css'
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
 const RotatingTorus = () => {
-  const torusRef = useRef(null);
+  const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const torusRef = useRef();
 
+  // Update size on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
   // Вращение торуса
   useFrame(() => {
     if (torusRef.current) {
-      torusRef.current.rotation.y += 0.005; // Вращение по оси Y
-      torusRef.current.rotation.x += 0.0025; // Вращение по оси X (по желанию)
+      torusRef.current.rotation.z += 0.0025;
     }
   });
 
@@ -58,15 +38,16 @@ const RotatingTorus = () => {
 
 export const TorusScene = () => {
   return (
-    <Canvas
-      camera={{ position: [10, -20, 20], fov: 75 }}
-      style={{ height: '600px', width: '700px' }}
-    >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+    <div className={styles.torus}>
+      <Canvas
+        camera={{ position: [10, -20, 20], fov: 75 }}
+      >
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
 
-      {/* Вращающийся торус */}
-      <RotatingTorus />
-    </Canvas>
+        {/* Вращающийся торус */}
+        <RotatingTorus />
+      </Canvas>
+    </div>
   );
 };
