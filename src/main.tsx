dc52e9472f81ea982f4d1583e96249f6e7c51052
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-09 00:33:56 +0300

    fix: HeartSection/Fix error with type any

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Создание корневого рендера
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-// Рендеринг приложения
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
---
+++ src/react-dom-client.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-dom/client' {
+  import { Root } from 'react-dom';
+
+  export function createRoot(container: Element | DocumentFragment): Root;
+}
