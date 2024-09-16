fdbc5b466712b397a9b73322eb51aabae949579b
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-17 00:37:45 +0300

    feat: MapAnimation/add animation for map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,22 +1,24 @@
 import { SubtextAnimation } from '../SubtextAnimation';
 import { TextAnimation } from '../TextAnimation';
 import styles from './styles.module.css';
+import {Country} from './Country';
 
 export const Map = () => {
   return (
 		<section className={styles.wrapper}>
 				<div className={styles.titleWrapper}>
-				<TextAnimation
-					text={[
-						'Хотите стать',
-						'одним из нас?'
-					]}
-          style={styles.title}
-				/>
-				<SubtextAnimation
-					subtext={'Приглашаем в команду'}
-					style={styles.description}
-				/>
+					<TextAnimation
+						text={[
+							'Хотите стать',
+							'одним из нас?'
+						]}
+						style={styles.title}
+					/>
+					<SubtextAnimation
+						subtext={'Приглашаем в команду'}
+						style={styles.description}
+					/>
+					<Country />
 				</div>
     </section>
   );
