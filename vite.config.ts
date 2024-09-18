c6372de9cfa61d807eae9c91a9c0830058abcfb9
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-09-18 21:09:33 +0300

    feat: add cache-reset

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-})
+  plugins: [
+    react(),
+    svgr(),
+    createHtmlPlugin({
+      inject: {
+        // Добавление мета-тегов в <head>
+        tags: [
+          {
+            tag: 'meta',
+            attrs: {
+              httpEquiv: 'Cache-Control',
+              content: 'no-cache, no-store, must-revalidate',
+            },
+          },
+          {
+            tag: 'meta',
+            attrs: {
+              httpEquiv: 'Pragma',
+              content: 'no-cache',
+            },
+          },
+          {
+            tag: 'meta',
+            attrs: {
+              httpEquiv: 'Expires',
+              content: '0',
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    headers: {
+      'Cache-Control': 'no-store', // Запретить кеширование
+    },
+  },
+});
